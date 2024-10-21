4e30ea82716bdf07c88e729ffbf71fc759cace13
Mariana Victorino <victorino.marii@gmail.com>
2024-10-21 16:36:46 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b); 
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if(pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if(this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  sum() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error("Empty SortedList");
+    }
+    return Math.min(...this.items);
 
-  avg() {}
-}
+  }
+
+  sum() {
+    if(this.items.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((acc, currentValue) => acc + currentValue);
+  }
+
+  avg() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
+};
 
 module.exports = SortedList;
